b2713953051d5a721316151221a4fce9e4e3ad4a
Jarikkomarik <ufland.ufland@gmail.com>
2021-08-16 14:46:35 +0200

    // unsused variable

--- src/main/java/calc/calcL.java
+++ src/main/java/calc/calcL.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class calcL {
     public static void main (String []args){
-        Scanner sc=new Scanner(System.in);
+        //Scanner sc=new Scanner(System.in);
         System.out.println("Welcome to calculator");         //Welcome
         System.out.println("--------ver2.0-------");
         
